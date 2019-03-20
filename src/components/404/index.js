3af61d84485e67ce52c39bd5054258a59409d460
Chris Yaxley <yaxley@studiojuice.com>
2019-03-20 16:36:42 +0000

    renaming the 404

--- src/components/404/index.js
+++ src/components/404/index.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import styles from './error.scss';
 
 class noMatch extends Component {
-
   componentDidUpdate() {
-    document.title = 'Studio Juice - 404';
+    document.title = 'Chris Yaxley - 404';
   }
+
   render() {
     return (
       <div className="site-content">
-        <div className={'container'}>
+        <div className="container">
           <div className={styles.errorContainer}>
             <span>4</span>
             <span>0</span>
