333de7510c26e5c27592e1d5100b27f5c1195ede
Chris Yaxley <yaxley@studiojuice.com>
2019-04-03 16:57:49 +0100

    Better 404

--- src/components/404/index.js
+++ src/components/404/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Header from '../CV/Header';
+import Footer from '../CV/Footer/container';
 import styles from './error.scss';
 
 class noMatch extends Component {
@@ -9,13 +11,16 @@ class noMatch extends Component {
   render() {
     return (
       <div className="site-content">
+       <Header />
         <div className="container">
           <div className={styles.errorContainer}>
-            <span>4</span>
-            <span>0</span>
-            <span>4</span>
+            <p>Sorry, I couldnt find what you are looking for. I hope you will except this image on me licking Timmy Mallet in 2005 as away of a apology</p>
+            <div className={styles.clipText}>
+            <p  className={styles.text}>404</p>
+            </div>
           </div>
         </div>
+        <Footer />
       </div>
     );
   }
