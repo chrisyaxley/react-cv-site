9a9c414225be13ad945dc71686ce9e635ee8dcbb
chrisyaxley <chris.yaxley@gmail.com>
2020-02-29 12:00:30 +0000

    Sorting my pervous roles into date order

--- src/components/CV/Experience/index.js
+++ src/components/CV/Experience/index.js
@@ -18,15 +18,25 @@ class Experience extends Component {
     props.getExperienceData();
   }
 
+  dateOrder(a, b) {
+    let comparison = 0;
+    if (a.fields.employedTo < b.fields.employedTo) {
+      comparison = 1;
+    } else if (a.fields.employedTo > b.fields.employedTo) {
+      comparison = -1;
+    }
+    return comparison;
+  }
+
   render() {
     const {
       loading, data,
     } = this.props;
-
+    console.log(data.sort(this.dateOrder));
     return (
       <section className={`fadeIn ${loading ? 'loading' : 'loaded'}`}>
         <h3 className="sectionHeader">Experience</h3>
-        {!loading && data.map(position => <Job key={position.sys.id} fields={position.fields} />)}
+        {!loading && data.sort(this.dateOrder).map(position => <Job key={position.sys.id} fields={position.fields} />)}
       </section>
     );
   }
